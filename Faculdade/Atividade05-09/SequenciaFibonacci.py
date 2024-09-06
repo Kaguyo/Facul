7401e9588b59bc24e2460e1ace6594a3e0ace262
Gabriel <gabrieltroop27@gmail.com>
2024-09-06 08:07:38 -0300

    Fibonacci

--- Faculdade/Atividade05-09/SequenciaFibonacci.py
+++ Faculdade/Atividade05-09/SequenciaFibonacci.py
@@ -1,11 +1,25 @@
-n = int(input())
-array = []
-fibonaccis = 0
-copia_fibonacci = 1
+n = int(input())  # Quantos fibonaccis o usuário quer
 
-for i in range(n):
-    array.append(fibonaccis)
-    fibonaccis, copia_fibonacci = copia_fibonacci, fibonaccis + copia_fibonacci
+fibonaccis = []
+if n == 1:
+    fibonaccis.append(0)
+elif n == 2:
+    fibonaccis.append(0)
+    fibonaccis.append(1)
+elif n > 2:
+    x = 1
+    fibonaccis.append(0)
+    fibonaccis.append(1)
+    for i in range(0, n - 2):
+        soma = fibonaccis[x] + fibonaccis[x - 1]
+        fibonaccis.append(soma)
+        x += 1
 
-# Converte cada número para string e imprime sem vírgulas
-print(' '.join(map(str, array)))
+#   map - percorrendo por todos valores contidos em fibonaccis e aplicando str() nesses mesmos valores.
+''' fibonaccis = ' '.join - agora que fibonaccis tornou-se um array de strings ([x, y, z]); o metodo join substitui une os valores tornando-se [xyz],
+    entretando, como usei ' ' junto ao metodo join, entre os valores unidos será colocado o valor que eu digitei. (' ') ou seja, deixando um espaço em branco.
+'''
+#   dessa maneira, o que antes era o array fibonaccis agora é uma variavel string, e seus valores é igual a todos valores de array fibonaccis em formato string,
+#   separado por espaços em branco agora
+fibonaccis = ' '.join(map(str, fibonaccis))
+print(fibonaccis)
